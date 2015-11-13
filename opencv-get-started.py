832664f5630eff87fef5f244918b4c4fa976e869
Mauricio Ribeiro <mauricioribeiro1992@gmail.com>
2015-11-13 20:18:38 -0200

    First commit :D
    simple face tracking. Works on Python 2.7 w/ Open cv 3.0.0

---
+++ opencv-get-started.py
@@ -0,0 +1,29 @@
+print('\n***** face recognition using open cv library - Mauricio Ribeiro *****\n')
+
+import cv2, os
+
+cascPath = os.getcwd() + '/opencv-3.0.0/data/haarcascade/'
+faceXml = 'haarcascade_frontalface_default.xml'
+faceCascade = cv2.CascadeClassifier(cascPath + faceXml)
+
+webcam  = cv2.VideoCapture(0)
+
+while True:
+	ret, frame = webcam.read()
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	scaleFactor = 1.3
+	minNeighbors = 5
+
+	faces = faceCascade.detectMultiScale(gray, scaleFactor, minNeighbors)
+
+	for (x, y, width, height) in faces:
+		cv2.retangle(frame, (x, y), (x + width, y + height), (0, 255,
+
+	cv2.imgshow('Face regognition record', frame)
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+webcam.release()
+cv2.destroyAllWindows()
+
