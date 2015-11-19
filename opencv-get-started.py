b018b9da4de4dcf338bf2096ec688201cedc9348
Mauricio Ribeiro <mauricioribeiro1992@gmail.com>
2015-11-19 17:44:46 -0200

    updating

--- opencv-get-started.py
+++ opencv-get-started.py
@@ -15,15 +15,16 @@ while True:
 	minNeighbors = 5
 
 	faces = faceCascade.detectMultiScale(gray, scaleFactor, minNeighbors)
+	color = (0, 255, 0)
+	thickness = 2
 
 	for (x, y, width, height) in faces:
-		cv2.retangle(frame, (x, y), (x + width, y + height), scaleFactor, minNeighbors)
+		cv2.rectangle(frame, (x, y), (x + width, y + height), color, thickness)
 
-	cv2.imgshow('Face recognition record', frame)
+	cv2.imshow('Face tracking record', frame)
 
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
 webcam.release()
 cv2.destroyAllWindows()
-
